02ac06f3bfcc404bf78ef821639409cbdcd2e940
shukriahmed-dd <shukri.ahmed@digital.cabinet-office.gov.uk>
2026-09-01 11:07:41 +0100

    chore: titleLabel is not an accessibility element

--- Sources/Components/GDSButton/GDSButton.swift
+++ Sources/Components/GDSButton/GDSButton.swift
@@ -49,6 +49,7 @@ public final class GDSButton: UIButton, ContentView {
         }
 
         self.shouldGroupAccessibilityChildren = true
+        self.titleLabel?.isAccessibilityElement = false
         if let accessibilityTraits = viewModel.accessibilityTraits {
             self.accessibilityTraits = accessibilityTraits
         }
